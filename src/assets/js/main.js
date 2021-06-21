b8ac509b5cdd675c9ef98efb066c9d8754b65272
malconn <malconaugusto@outlook.com>
2021-06-21 11:32:34 -0300

    build and run

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,2 +1,8 @@
-const teste = 'teste'
-console.log(teste);
+const bgHeader = document.querySelector("header");
+const btNav = document.querySelector(".navbar-toggler");
+const icon = document.querySelector(".fa-bars");
+
+btNav.addEventListener("click", () => {
+    bgHeader.classList.toggle("active");
+    icon.classList.toggle("fa-times");
+})
